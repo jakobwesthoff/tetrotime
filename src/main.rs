d8b2d44c57c3cf8ae1741b9ffe1989e3fd251ba3
Jakob Westhoff <jakob@westhoffswelt.de>
2024-11-10 17:18:44 +0100

    Implemented stopwatch and countdown modes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod tetromino;
 
 use animation::Digit;
 use anyhow::Result;
-use chrono::{DateTime, Duration, Local};
+use chrono::{DateTime, Duration, Local, NaiveTime, Timelike};
 use clap::{arg, Parser};
 use pixel_loop::canvas::CrosstermCanvas;
 use pixel_loop::canvas::{Canvas, RenderableCanvas};
@@ -11,12 +11,51 @@ use pixel_loop::color::Color;
 use pixel_loop::input::{CrosstermInputState, KeyboardKey, KeyboardState};
 use tetromino::{Board, Colorscheme, DigitBoard, Rotation, Shape};
 
-fn time_to_digits(now: DateTime<Local>) -> Vec<Digit> {
-    let now_string = now.format("%H%M%S").to_string();
-    now_string
+fn time_string_to_digits<T: AsRef<str>>(time_string: T) -> Vec<Digit> {
+    time_string
+        .as_ref()
         .chars()
         .map(|c| Digit::from(c.to_digit(10).unwrap()))
         .collect()
+}
+
+#[derive(Debug, Clone)]
+enum Mode {
+    Clock,
+    Countdown(DateTime<Local>),
+    Stopwatch(DateTime<Local>),
+}
+
+impl Default for Mode {
+    fn default() -> Self {
+        Self::Clock
+    }
+}
+
+impl Mode {
+    fn get_timestring(&self) -> String {
+        match self {
+            Self::Clock => Local::now().format("%H%M%S").to_string(),
+            Self::Countdown(end) => {
+                let duration = end.signed_duration_since(Local::now());
+                // negative duration simply returns 000000
+                if duration.num_seconds() < 0 {
+                    return "000000".to_string();
+                }
+                let hours = duration.num_hours();
+                let minutes = duration.num_minutes() % 60;
+                let seconds = duration.num_seconds() % 60;
+                format!("{:02}{:02}{:02}", hours, minutes, seconds)
+            }
+            Self::Stopwatch(start) => {
+                let duration = Local::now().signed_duration_since(*start);
+                let hours = duration.num_hours();
+                let minutes = duration.num_minutes() % 60;
+                let seconds = duration.num_seconds() % 60;
+                format!("{:02}{:02}{:02}", hours, minutes, seconds)
+            }
+        }
+    }
 }
 
 struct State {
@@ -25,15 +64,17 @@ struct State {
     seperator_boards: Vec<Board>,
     last_update_time: DateTime<Local>,
     colorscheme: Colorscheme,
+    mode: Mode,
 }
 
 impl State {
-    fn new(colorscheme: Colorscheme) -> Self {
+    fn new(mode: Mode, colorscheme: Colorscheme) -> Self {
         Self {
             digit_boards: vec![],
             current_digits: vec![],
             seperator_boards: vec![],
             last_update_time: Local::now(),
+            mode,
             colorscheme,
         }
     }
@@ -47,7 +88,7 @@ impl State {
         let x_start = (width as i64 - 6 * 6 - 5 * 2) / 2;
         let y_stop = (height as i64 + 10) / 2;
         let now = Local::now();
-        let digits = time_to_digits(now);
+        let digits = time_string_to_digits(self.mode.get_timestring());
 
         let colorscheme = self.colorscheme.clone();
         self.digit_boards = digits
@@ -88,8 +129,7 @@ impl State {
         }
     }
 
-    fn update_time(&mut self, now: DateTime<Local>) {
-        let digits = time_to_digits(now);
+    fn update_time(&mut self, digits: Vec<Digit>) {
         for (i, board) in self.digit_boards.iter_mut().enumerate() {
             if self.current_digits[i] != digits[i] {
                 board.set_digit(digits[i]);
@@ -104,17 +144,49 @@ impl State {
     about = "TetroTime - Time meets Tetris!"
 )]
 struct Args {
-    #[arg(short='c', long, value_enum, default_value_t = Colorscheme::default())]
+    #[arg(short = 'c', long, group = "mode", help = "Show a clock")]
+    clock: bool,
+    #[arg(short = 'w', long, group = "mode", help = "Show a stopwatch")]
+    stopwatch: bool,
+    #[arg(
+        short = 'd',
+        long,
+        group = "mode",
+        help = "Show a countdown (Duration in HHMMSS or HH:MM:SS)",
+        value_name = "DURATION"
+    )]
+    countdown: Option<String>,
+    #[arg(short='s', long, value_enum, default_value_t = Colorscheme::default(), help = "Select a specific colorscheme")]
     colorscheme: Colorscheme,
+}
+
+fn get_mode_from_args(args: &Args) -> Result<Mode> {
+    if args.clock {
+        Ok(Mode::Clock)
+    } else if args.stopwatch {
+        Ok(Mode::Stopwatch(Local::now()))
+    } else if let Some(countdown) = &args.countdown {
+        let time = NaiveTime::parse_from_str(countdown, "%H:%M:%S").unwrap_or_else(|_| {
+            NaiveTime::parse_from_str(countdown, "%H%M%S")
+                .unwrap_or(NaiveTime::from_num_seconds_from_midnight_opt(0, 0).unwrap())
+        });
+        Ok(Mode::Countdown(
+            Local::now() + Duration::seconds(time.num_seconds_from_midnight() as i64),
+        ))
+    } else {
+        Ok(Mode::default())
+    }
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
+    let mode = get_mode_from_args(&args)?;
+
     let canvas = CrosstermCanvas::new();
     let input = CrosstermInputState::new();
 
-    let mut state = State::new(args.colorscheme);
+    let mut state = State::new(mode, args.colorscheme);
     state.resize_canvas(canvas.width(), canvas.height());
 
     eprintln!("Render size: {}x{}", canvas.width(), canvas.height());
@@ -143,7 +215,7 @@ fn main() -> Result<()> {
 
             let now = Local::now();
             if now.signed_duration_since(s.last_update_time) > Duration::seconds(5) {
-                s.update_time(now);
+                s.update_time(time_string_to_digits(s.mode.get_timestring()));
                 s.last_update_time = now;
             }
 
